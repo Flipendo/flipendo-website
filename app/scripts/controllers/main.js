26e5846a60a972c4d97d032a061b8a580fc365b0
Julien Fouilhé <julien.fouilhe@gmail.com>
2015-05-02 23:17:49 +1000

    Add animation for success

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,14 +8,32 @@
  * Controller of the flipendoApp
  */
 angular.module('flipendoApp')
-  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('MainCtrl', ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
+    $scope.chunks = [];
+    $scope.done = false;
+
     $http.get('/datasets/chunks_test.json').success(function(data) {
       $scope.chunks = data;
     });
+
+    var appear;
+    var disappear;
+
+    disappear = function() {
+      $scope.done = false;
+      $timeout(appear, 3000);
+    };
+    appear = function() {
+      $scope.done = true;
+      $timeout(disappear, 3000);
+    };
+
+    $timeout(appear, 3000);
   }])
   .directive('progressCircle', function() {
     return {
       scope: {
+        done: '=',
         chunks:   '=',
         progress: '=progress'
       },
